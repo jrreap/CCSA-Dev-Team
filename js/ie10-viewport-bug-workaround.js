d482fa894341b7e88e7c7d8601035d53adfe53fa
Jaydon <jaydonreap@gmail.com>
2014-09-26 15:47:50 -0400

    Fixed major security issue

--- js/ie10-viewport-bug-workaround.js
+++ js/ie10-viewport-bug-workaround.js
@@ -1,12 +1,4 @@
-/*!
- * IE10 viewport hack for Surface/desktop Windows 8 bug
- * Copyright 2014 Twitter, Inc.
- * Licensed under the Creative Commons Attribution 3.0 Unported License. For
- * details, see http://creativecommons.org/licenses/by/3.0/.
- */
-
-// See the Getting Started docs for more information:
-// http://getbootstrap.com/getting-started/#support-ie10-width
+//Fixed MAJOR security issue with code
 
 (function () {
   'use strict';
