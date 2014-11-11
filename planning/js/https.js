95ae550e1282d32123164f64e1b465492d31ee96
jrreap <jaydonreap@gmail.com>
2014-11-10 21:59:04 -0500

    Lots of Updates and formatting updates

---
+++ planning/js/https.js
@@ -0,0 +1,5 @@
+var el = document.getElementById("clickMe");
+if (el.addEventListener)
+    el.addEventListener("click", doFunction, false);
+else if (el.attachEvent)
+    el.attachEvent('onclick', doFunction);
