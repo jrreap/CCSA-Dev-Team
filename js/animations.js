2fa9effd62438061a6791156c8a1a726ce3ace93
jrreap <jaydonreap@gmail.com>
2014-11-20 20:11:23 -0500

    Main animation class for the API

---
+++ js/animations.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(document).ready(function (){
+$('body').css('opacity', '0').fadeTo(5000, 1,'swing'); 
+});
+
+$(document).ready(function (){
+$('#a img').css('opacity', '0').fadeTo(5000, 1,'swing'); 
+});
