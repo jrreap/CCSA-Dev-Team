8a5f76c5c678ff55ecb59c40221e15fae644c6fb
jrreap <jaydonreap@gmail.com>
2014-11-24 16:06:51 -0500

    Added more animation types

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,24 @@
 // JavaScript Document
 $(document).ready(function (){
-$('body').css('opacity', '0').fadeTo(5000, 1,'swing'); 
+$('body').css('opacity', '0').fadeTo(4000, 1,'swing'); 
 });
 
-$(document).ready(function (){
-$('#a').css('opacity', '0').fadeTo(3000, 1,'swing'); 
+$(document).ready(function(){
+    $("#fade").hide(0).delay(2000).fadeIn(3000)
+});
+
+$(document).ready(function(){
+    $("#fade_last").hide(0).delay(3500).fadeIn(3000)
+});
+
+$(document).ready(function(){
+    $("#fade1").hide(0).delay(2000).fadeIn(1500)
+});
+
+$(document).ready(function(){
+    $("#fade2").hide(0).delay(2500).fadeIn(1500)
+});
+
+$(document).ready(function(){
+    $("#fade3").hide(0).delay(3000).fadeIn(1500)
 });
