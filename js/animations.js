6c0fb7a08d223e7224e171e95d46929edba3906f
Jaydon <jaydonreap@gmail.com>
2014-12-09 14:36:43 -0500

    Updated animation speeds

--- js/animations.js
+++ js/animations.js
@@ -1,14 +1,14 @@
 // JavaScript Document
 $(document).ready(function (){
-$('body').css('opacity', '0').fadeTo(2500, 1,'swing'); 
+$('body').css('opacity', '0').fadeTo(4000, 1,'swing'); 
 });
 
 $(document).ready(function(){
-    $("#fade").hide(0).delay(2000).fadeIn(3000)
+    $("#fade").hide(0).delay(2000).fadeIn(2500)
 });
 
 $(document).ready(function(){
-    $("#fade_last").hide(0).delay(3500).fadeIn(3000)
+    $("#fade_last").hide(0).delay(3500).fadeIn(2500)
 });
 
 $(document).ready(function(){
