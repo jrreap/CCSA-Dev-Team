82bd36a57ef2548b6b556339dde0554e74b3145a
jrreap <jaydonreap@gmail.com>
2014-12-10 20:51:27 -0500

    Updated browser detection script system

--- js/brsdet.js
+++ js/brsdet.js
@@ -5,17 +5,9 @@
 function whichBrs() {
 var agt=navigator.userAgent.toLowerCase();
 if (agt.indexOf("opera") != -1) return 'Opera';
-if (agt.indexOf("staroffice") != -1) return 'Star Office';
-if (agt.indexOf("webtv") != -1) return 'WebTV';
-if (agt.indexOf("beonex") != -1) return 'Beonex';
-if (agt.indexOf("chimera") != -1) return 'Chimera';
-if (agt.indexOf("netpositive") != -1) return 'NetPositive';
-if (agt.indexOf("phoenix") != -1) return 'Phoenix';
 if (agt.indexOf("firefox") != -1) return 'Firefox';
 if (agt.indexOf("safari") != -1) return 'Safari';
-if (agt.indexOf("skipstone") != -1) return 'SkipStone';
 if (agt.indexOf("msie") != -1) return 'Internet Explorer';
-if (agt.indexOf("netscape") != -1) return 'Netscape';
 if (agt.indexOf("mozilla/5.0") != -1) return 'Mozilla';
 if (agt.indexOf('\/') != -1) {
 if (agt.substr(0,agt.indexOf('\/')) != 'mozilla') {
